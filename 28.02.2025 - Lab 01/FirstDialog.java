ea894297cfffa0ad06d4402d28e9a6310a4e9f84
nguyenducthinh07022005 <thinhthongminh.duc.thcspt@gmail.com>
2025-05-30 22:00:01 +0700

    Delete 28.02.2025 - Lab 01 directory

--- 28.02.2025 - Lab 01/FirstDialog.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JOptionPane;
-public class FirstDialog{
-    public static void main(String args[]){
-        JOptionPane.showMessageDialog(null, "Hello World, How Are You?");
-        System.exit(0);
-    }
-}
